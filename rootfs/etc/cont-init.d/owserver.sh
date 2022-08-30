062c171994eece5225b5001d8910bd168b84d597
John Arild Berentsen <turbokongen@hotmail.com>
2022-08-30 20:18:39 +0200

    Full configurable device
    This allow to use usb, passive etc....

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -7,7 +7,7 @@ device=$(bashio::config 'device')
 if bashio::config.has_value 'device' ;then
     bashio::log.info "Configuring ${device} device"
     device=$(bashio::string.replace ${device} '/' '\/')
-    sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
+    sed -i "s/%%device%%/${device}/g" /etc/owfs.conf
 else
     bashio::log.info "Configuring fake device"
     sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
